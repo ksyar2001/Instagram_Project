4842e494e7c2e7c46a85f283871514786c32536a
Sung H Kang <SunnyK@Sungs-MacBook-Pro.local>
2017-02-18 01:19:49 -0600

    django templating example

--- webpage/views.py
+++ webpage/views.py
@@ -8,9 +8,8 @@ def callback(request):
     return HttpResponse("INSTAGRAM TOKEN HERE")
 
 def rendering(request):
-
-
-	return render(request, "base.html")
+	data_list = [4, 8, 15, 16, 23, 42, 60, 120, 240, 450]
+	return render(request, "base.html", {'data': data_list})
 
 
 	
