268d6023023fe7a85c7dfc2f7d464fc264c24941
PYangDizzle <pyang43@wisc.edu>
2017-02-18 19:00:20 -0600

    added PI, TA classes to organize the services within WatsonAPI

--- watson/WatsonAPI.py
+++ watson/WatsonAPI.py
@@ -1,28 +1,60 @@
 # communicates with Watson API
 
-from watson_developer_cloud import PersonalityInsightsV3
-
-# Sends text to Watson's Personality Insigts service
-# then receive analysis.
+from watson_developer_cloud import PersonalityInsightsV3, ToneAnalyzerV3
+import json
 
 class WatsonAPI():
-    default_username = 'cd3e87ef-1985-46b1-8726-b41d4f935973'
-    default_password = 'aHcW3DV4nWyk'
-    personality_insights = None
-
-    def __init__( self, username=default_username, password=default_password):
-        self.personality_insights = PersonalityInsightsV3(
-            username = username,
-            password = password
-        )
+    def __init__( self ):
+        self.PI = Personality()
+        self.TA = Tone()
     
     def getPersonalityInsights( self, text ):
-        return self.personality_insights.profile( 
+        return self.PI.profile( 
             text = text, # input to be analyzed
             raw_scores = True, # raw_scores in addition to percentile
         )
 
-test = WatsonAPI()
-test.getPersonalityInsights( """
+    def getToneAnalysis( self, text ):
+        return self.TA.tone( 
+            text = text # input to be analyzed
+        )
 
-""" )
+
+# Sends text to Watson's Personality Insigts service
+# then receive analysis.
+
+class Personality():
+    default_username = 'cd3e87ef-1985-46b1-8726-b41d4f935973'
+    default_password = 'aHcW3DV4nWyk'
+
+    def __init__( self, username = default_username, password = default_password ):
+        self.personality_insights = PersonalityInsightsV3(
+            username = username,
+            password = password
+        )
+
+    def profile( self, text, raw_scores ):
+        return self.personality_insights.profile( 
+            text = text,
+            raw_scores = raw_scores
+        )
+
+class Tone():
+    default_username = "2638a3bc-0207-4aeb-aa8f-232c1db2ec74"
+    default_password = "qOUkOykTwwlu"
+
+    def __init__( self, username = default_username, password = default_password ):
+        self.tone_analyzer = ToneAnalyzerV3(
+            username = username,
+            password = password,
+            version = '2016-05-19'
+        )
+
+    def tone( self, text ):
+        return self.tone_analyzer.tone( 
+            text = text 
+        )
+
+
+test = WatsonAPI()
+print( json.dumps( test.getToneAnalysis( "I love you" ), indent = 2 ) )
